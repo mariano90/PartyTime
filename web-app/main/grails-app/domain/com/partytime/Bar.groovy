befbda31f0ba3822a202565d7dd89614b5855252
Fabricio PH <fabriph@gmail.com>
2014-06-01 20:32:58 -0300

    Small change

--- web-app/main/grails-app/domain/com/partytime/Bar.groovy
+++ web-app/main/grails-app/domain/com/partytime/Bar.groovy
@@ -1,13 +1,15 @@
 package com.partytime
 
 class Bar {
+	String name
     String openHours
     String promos
     String address
     Review reviews // TODO: do a list
-    MusicStyle musicStyles
+    static hasMany = [musicStyles: MusicStyle]
     int minimumAge
 
     static constraints = {
+		reviews nullable: true
     }
 }
