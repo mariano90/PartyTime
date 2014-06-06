be8cbf9867197e4a23d3008e2008209b34f048fa
Fabricio PH <fabriph@gmail.com>
2014-06-06 00:53:50 -0300

    Example of data loaded on startup

--- web-app/main/grails-app/conf/BootStrap.groovy
+++ web-app/main/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,12 @@
+import com.partytime.Component
+import com.partytime.Ingredient
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def init = { servletContext ->
+		Ingredient ingredient1 = new Ingredient(name: "nam1", price: 1.0).save()
+		Component component1 = new Component(ingredient: ingredient1, amount: "1 L").save()
+	}
+	def destroy = {
+	}
 }
