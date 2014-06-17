16231e5f29effb2bc6130588e2af9488638a7880
Fabricio PH <fabriph@gmail.com>
2014-06-17 15:28:45 -0300

    Test data for Place and Review

--- web-app/main/grails-app/conf/BootStrap.groovy
+++ web-app/main/grails-app/conf/BootStrap.groovy
@@ -2,6 +2,8 @@ import com.partytime.Component
 import com.partytime.Drink
 import com.partytime.Ingredient
 import com.partytime.MusicStyle
+import com.partytime.Place
+import com.partytime.Review
 import com.partytime.User
 
 class BootStrap {
@@ -39,10 +41,17 @@ class BootStrap {
 		MusicStyle musicStyleElectro = new MusicStyle(name: "Electro").save()
 		MusicStyle musicStyleReggaeton = new MusicStyle(name: "Reggaeton").save()
 
-
 		User user1 = new User(name: "User1", bornDate: new Date()).save()
 		User user2 = new User(name: "User2", bornDate: new Date()).save()
 		User user3 = new User(name: "User3", bornDate: new Date()).save()
+
+		Review review1 = new Review(title:"review1", body:"body1", score:4, author: user1).save()
+		Review review2 = new Review(title:"review2", body:"body2", score:4, author: user1).save()
+		Review review3 = new Review(title:"review3", body:"body3", score:5, author: user2).save()
+
+		Place place1 = new Place(name: "place1", minimumAge: 18, street: "Av Santa Fe", number:"1234", city:"CABA", reviews: [review1, review3]).save()
+		Place place2 = new Place(name: "place2", minimumAge: 18, street: "Av Santa Fe", number:"1111", city:"CABA", reviews: [review2]).save()
+		Place place3 = new Place(name: "place3", minimumAge: 21, street: "Av Cordoba", number:"1234", city:"CABA", reviews: []).save()
 	}
 	def destroy = {
 	}
