c64d29415c4ac85aead6c1ebcf62ddfb11223944
Fabricio PH <fabriph@gmail.com>
2014-06-22 23:02:39 -0300

    Working on party controller and servicies

--- web-app/main/grails-app/controllers/com/partytime/PartyController.groovy
+++ web-app/main/grails-app/controllers/com/partytime/PartyController.groovy
@@ -1,16 +1,41 @@
 package com.partytime
 
 class PartyController {
+	
+	//def partyService
 
 	def index() {
 	}
 
 	def create() {
+		boolean isQuickParty = true
+
+		// TODO fetch all this
+		String description = ""
+		User owner = null
+		Date startDate = null
+		Date endDate = null
+		Set guests = []
+		Place place = null
+
+		if (isQuickParty) {
+			// TODO fetch
+			String meetingPoint = null
+			new QuickParty(meetingPoint: meetingPoint).save()
+		} else {
+			// TODO fetch
+			Set drinks = []
+			// TODO fetch
+			User dj = null // TODO check availability on selection
+			// TODO fetch
+			User barman = null // TODO check availability on selection
+			new ScheduledParty(drinks: drinks, dj: dj, barman:barman).save()
+		}
 		render "new party was saved"
 	}
 
 	def all() {
-		user = User.findByName("Fabricio")
+		User user = User.findByName("Fabricio")
 		render user
 	}
 }
