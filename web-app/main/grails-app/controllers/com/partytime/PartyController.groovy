6f9a91d7a19cb0fe1c1efb9939f67393dd3ecf1c
Fabricio PH <fabriph@gmail.com>
2015-01-10 18:31:24 -0300

    Party Controller: some small edits.

--- web-app/main/grails-app/controllers/com/partytime/PartyController.groovy
+++ web-app/main/grails-app/controllers/com/partytime/PartyController.groovy
@@ -1,16 +1,123 @@
 package com.partytime
 
-class PartyController {
 
-	def index() {
+
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class PartyController {
+/*
+ def index() {
+		User user = User.findById(params.user)
+		//render "All parties of ${user}<br/>"
+		Set parties = Party.findAllWhere("host": user)
+		//render "${parties}"
 	}
 
 	def create() {
-		render "new party was saved"
+		User user = User.findById(params.user)
+		render "hola"
 	}
 
-	def all() {
-		user = User.findByName("Fabricio")
-		render user
+	def edit() {
+		render "TODO: edit party"
 	}
+
+	def save() {
+		render "TODO: new party was saved"
+	}
+ */
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        params.max = Math.min(max ?: 10, 100)
+        respond Party.list(params), model:[partyInstanceCount: Party.count()]
+    }
+
+    def show(Party partyInstance) {
+        respond partyInstance
+    }
+
+    def create() {
+    }
+
+    @Transactional
+    def save(Party partyInstance) {
+        if (partyInstance == null) {
+            notFound()
+            return
+        }
+
+        if (partyInstance.hasErrors()) {
+            respond partyInstance.errors, view:'create'
+            return
+        }
+
+        partyInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'party.label', default: 'Party'), partyInstance.id])
+                redirect partyInstance
+            }
+            '*' { respond partyInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Party partyInstance) {
+        respond partyInstance
+    }
+
+    @Transactional
+    def update(Party partyInstance) {
+        if (partyInstance == null) {
+            notFound()
+            return
+        }
+
+        if (partyInstance.hasErrors()) {
+            respond partyInstance.errors, view:'edit'
+            return
+        }
+
+        partyInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Party.label', default: 'Party'), partyInstance.id])
+                redirect partyInstance
+            }
+            '*'{ respond partyInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Party partyInstance) {
+
+        if (partyInstance == null) {
+            notFound()
+            return
+        }
+
+        partyInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Party.label', default: 'Party'), partyInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'party.label', default: 'Party'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
 }
