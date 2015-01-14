3aa36a1882940892ede38cdf85a1ae2973d9001e
Fabricio PH <fabriph@gmail.com>
2015-01-13 21:18:15 -0300

    Rolling back.

--- web-app/main/grails-app/controllers/com/partytime/PromoController.groovy
+++
@@ -1,5 +0,0 @@
-package com.partytime
-
-class PromoController {
-    static scaffold = true
-}
--- web-app/main/grails-app/controllers/main/HomeController.groovy
+++
@@ -1,8 +0,0 @@
-package main
-
-class HomeController {
-
-    def index() {
-		render view:"home.gsp"
-    }
-}
