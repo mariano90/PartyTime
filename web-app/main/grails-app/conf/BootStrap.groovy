f1fb9082c2871b5d6c19eb7ec3cdc0a72a57654e
Fabricio PH <fabriph@gmail.com>
2015-01-24 21:07:59 -0300

    Code style.

--- web-app/main/grails-app/conf/BootStrap.groovy
+++ web-app/main/grails-app/conf/BootStrap.groovy
@@ -58,125 +58,195 @@ class BootStrap {
 		MusicStyle musicStyleUnder = new MusicStyle(name: "Under").save(failOnError: true)
 		MusicStyle musicStyle80 = new MusicStyle(name: "80's").save(failOnError: true)
 
-		User userf = new User(name: "fabricio", bornDate: new Date()).save(failOnError: true)
-		// TODO: link mode users with plugin authentication users.
-		//		assert new AuthenticationUser( login:'fabricio', password:'123456'.encodeAsMD5(), email:'someone@somewhere.com',
-		//		status:AuthenticationService.STATUS_VALID).save()
+		User userf = new User(name: "fabricio",
+			bornDate: new Date()
+			).save(failOnError: true)
 		userf.preferedDrinks.add(tropicalEnergy)
 		userf.preferedDrinks.add(acidPsycho)
 		userf.save()
+		// TODO: link mode users with plugin authentication users.
+		//      assert new AuthenticationUser( login:'fabricio',password:'123456'.encodeAsMD5(),email:'someone@somewhere.com',
+
+		//      status:AuthenticationService.STATUS_VALID).save()
 		
-		User userm = new User(name: "mariano", bornDate: new Date()).save(failOnError: true)
-		User user1 = new User(name: "user1", bornDate: new Date()).save(failOnError: true)
-		User user2 = new User(name: "user2", bornDate: new Date()).save(failOnError: true)
-		User user3 = new User(name: "user3", bornDate: new Date()).save(failOnError: true)
-		User user4 = new User(name: "user4", bornDate: new Date()).save(failOnError: true)
-		User user5 = new User(name: "user5", bornDate: new Date()).save(failOnError: true)
+		
+		User userm = new User(name: "mariano",
+			bornDate: new Date()).save(failOnError: true)
+		User user1 = new User(name: "user1",
+			bornDate: new Date()).save(failOnError: true)
+		User user2 = new User(name: "user2",
+			bornDate: new Date()).save(failOnError: true)
+		User user3 = new User(name: "user3",
+			bornDate: new Date()).save(failOnError: true)
+		User user4 = new User(name: "user4",
+			bornDate: new Date()).save(failOnError: true)
+		User user5 = new User(name: "user5",
+			bornDate: new Date()).save(failOnError: true)
 
 		userm.preferedDrinks.add(fernetConCoca)
 		userm.preferedDrinks.add(cerveza)
 		userm.save()
 
-		Review review1 = new Review(title:"Bueno en pareja", body:"Fue una increible salida conmi novia, y eso que soy informatico.", score:4, author: user1).save(failOnError: true)
-		Review review2 = new Review(title:"Me gusta el ron", body:"Fuimos al lugar disfrazados de piratas, que mejor sorpresa que ordenar un buen ron y disfrutar el botin todos juntos, arrrghhh.", score:4, author: user1).save(failOnError: true)
-		Review review3 = new Review(title:"Lo vi a Harry Potter!", body:"Al entrar al lugar note alguien haciendo magia, fue una noche increible con el show de luces.", score:5, author: user2).save(failOnError: true)
+		Review review1 = new Review(title:"Bueno en pareja",
+			body:"Fue una increible salida conmi novia, y eso que soy informatico.",
+			score:4,
+			author: user1).save(failOnError: true)
+		Review review2 = new Review(title:"Me gusta el ron",
+			body:"Fuimos al lugar disfrazados de piratas, que mejor sorpresa que ordenar un buen ron y disfrutar el botin todos juntos, arrrghhh.",
+			score:4,
+			author: user1).save(failOnError: true)
+		Review review3 = new Review(title:"Lo vi a Harry Potter!",
+			body:"Al entrar al lugar note alguien haciendo magia, fue una noche increible con el show de luces.",
+			score:5,
+			author: user2).save(failOnError: true)
 
-		Promo promo1 = new Promo(title: "2x1 en tragos seleccionados", description: "toda la noche", drink: fernetConCoca).save(failOnError: true)
-		Promo promo2 = new Promo(title: "Chicas gratis hasta 2:00 AM", description: "").save(failOnError: true)
+		Promo promo1 = new Promo(title: "2x1 en tragos seleccionados",
+			description: "toda la noche",
+			drink: fernetConCoca).save(failOnError: true)
+		Promo promo2 = new Promo(title: "Chicas gratis hasta 2:00 AM",
+			description: "").save(failOnError: true)
 
-		Bar place0 = new Bar(name: "Groove", minimumAge: 0, street: "-", number:"0", city:"-", reviews: []).save(failOnError: true)
-		Bar place1 = new Bar(name: "Brook on brails", minimumAge: 18, street: "Av Santa Fe", number:"1234", city:"CABA", reviews: [review1, review3]).save(failOnError: true)
-		Bar place2 = new Bar(name: "El rincon del mal", minimumAge: 18, street: "Av Santa Fe", number:"1111", city:"CABA", reviews: [review2], promos: [promo1, promo2]).save(failOnError: true)
-		Bar place3 = new Bar(name: "Mucha-chica", minimumAge: 21, street: "Av Cordoba", number:"1234", city:"CABA", reviews: [], promos: [promo2]).save(failOnError: true)
-		Bar place4 = new Bar(name: "La casa embrujada de Palermo", minimumAge: 21, street: "Av Corrientes", number:"1234", city:"CABA", reviews: [], promos: []).save(failOnError: true)
+		Bar place0 = new Bar(name: "Groove",
+			minimumAge: 0,
+			street: "-",
+			number:"0",
+			city:"-",
+			reviews: []).save(failOnError: true)
+		Bar place1 = new Bar(name: "Brook on brails",
+			minimumAge: 18,
+			street: "Av Santa Fe",
+			number:"1234",
+			city:"CABA",
+			reviews: [review1, review3]
+			).save(failOnError: true)
+		Bar place2 = new Bar(name: "El rincon del mal",
+			minimumAge: 18,
+			street: "Av Santa Fe",
+			number:"1111",
+			city:"CABA",
+			reviews: [review2],
+			promos: [promo1, promo2]
+			).save(failOnError: true)
+		Bar place3 = new Bar(name: "Mucha-chica",
+			minimumAge: 21,
+			street: "Av Cordoba",
+			number:"1234",
+			city:"CABA",
+			reviews: [],
+			promos: [promo2]).save(failOnError: true)
+		Bar place4 = new Bar(name: "La casa embrujada de Palermo",
+			minimumAge: 21,
+			street: "Av Corrientes",
+			number:"1234",
+			city:"CABA",
+			reviews: [],
+			promos: []).save(failOnError: true)
 
-		new News(title:"Lanzamos Party Time!", description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",date: new Date(1429066800000L), imageUrl:"").save(failOnError: true)
-		new News(title:"Soportamos iPhone y Android", description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",date: new Date(1428066810000L), imageUrl:"").save(failOnError: true)
-		new News(title:"Aniversario Party Time, 50% descuento en todo!", description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",date: new Date(1427066820000L), imageUrl:"").save(failOnError: true)
-		new News(title:"Noticia 4", description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",date: new Date(1426066830000L), imageUrl:"").save(failOnError: true)
+		new News(title:"Lanzamos Party Time!",
+			description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",
+			date: new Date(1429066800000L),
+			imageUrl:"").save(failOnError: true)
+		new News(title:"Soportamos iPhone y Android",
+			description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",
+			date: new Date(1428066810000L),
+			imageUrl:"").save(failOnError: true)
+		new News(title:"Aniversario Party Time, 50% descuento en todo!",
+			description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",
+			date: new Date(1427066820000L),
+			imageUrl:"").save(failOnError: true)
+		new News(
+			title:"Noticia 4",
+			description: "Lorem ipsum dolor sit amet, vim ad affert putant verear. Accusata iracundia pro id.",
+			date: new Date(1426066830000L),
+			imageUrl:""
+			).save(failOnError: true)
 
 		new PublicEvent(
-				title:"Creamfields",
-				description:"La fiesta de musica electronica mas famosa del mundo",
-				lineup:"Tiesto, Axwel, Swedish House Mafia",
-				minimumAge:18,
-				startDateTime: new Date(1429067000000L),
-				externalUrl:"http://www.creamfields.com/",
-				address:"Puerto Madero, CABA"
-				).save(failOnError: true)
+			title:"Creamfields",
+			description:"La fiesta de musica electronica mas famosa del mundo",
+			lineup:"Tiesto, Axwel, Swedish House Mafia",
+			minimumAge:18,
+			startDateTime: new Date(1429067000000L),
+			externalUrl:"http://www.creamfields.com/",
+			address:"Puerto Madero, CABA"
+			).save(failOnError: true)
 
 		new PublicEvent(
-				title:"Ultramusic Festival",
-				description:"",
-				lineup:"Pinion Fijo, Bala, y balas",
-				minimumAge:18,
-				startDateTime: new Date(1429066100000L),
-				externalUrl:"",
-				address:"Autodromo, CABA"
-				).save(failOnError: true)
+			title:"Ultramusic Festival",
+			description:"",
+			lineup:"Pinion Fijo, Bala, y balas",
+			minimumAge:18,
+			startDateTime: new Date(1429066100000L),
+			externalUrl:"",
+			address:"Autodromo, CABA"
+			).save(failOnError: true)
 
 		new PublicEvent(
-				title:"Tomorrowland",
-				description:"DJs lideres",
-				lineup:"",
-				minimumAge:0,
-				startDateTime: new Date(1429066200000L),
-				externalUrl:"http://www.tomorrowland.com/global-splash/",
-				address:"Belgium"
-				).save(failOnError: true)
+			title:"Tomorrowland",
+			description:"DJs lideres",
+			lineup:"",
+			minimumAge:0,
+			startDateTime: new Date(1429066200000L),
+			externalUrl:"http://www.tomorrowland.com/global-splash/",
+			address:"Belgium"
+			).save(failOnError: true)
 
 		new PublicEvent(
-				title:"Cirque du Soleil",
-				description:"Veni a ver Ka, el show mas espectacular de circo a nivel mundial.",
-				lineup:"",
-				minimumAge:0,
-				startDateTime: new Date(1429066890000L),
-				externalUrl:"http://www.cirquedusoleil.com",
-				address:"Teatro Colon, CABA"
-				).save(failOnError: true)
+			title:"Cirque du Soleil",
+			description:"Veni a ver Ka, el show mas espectacular de circo a nivel mundial.",
+			lineup:"",
+			minimumAge:0,
+			startDateTime: new Date(1429066890000L),
+			externalUrl:"http://www.cirquedusoleil.com",
+			address:"Teatro Colon, CABA"
+			).save(failOnError: true)
 
 		new PublicEvent(
-				title:"David Copperfield",
-				description:"¿Te gusta la magia?",
-				lineup:"",
-				minimumAge:9,
-				startDateTime: new Date(1429066800000L),
-				externalUrl:"",
-				address:"Las Vegas, NV, USA"
-				).save(failOnError: true)
+			title:"David Copperfield",
+			description:"¿Te gusta la magia?",
+			lineup:"",
+			minimumAge:9,
+			startDateTime: new Date(1429066800000L),
+			externalUrl:"",
+			address:"Las Vegas, NV, USA"
+			).save(failOnError: true)
 
 
 		new Party(host: userm,
-		place: place1,
-		title: "Fiesta1",
-		description: "...",
-		startDateTime: new Date(1429066800000L),
-		finsishDateTime: new Date(1429066900000L),
-		guests: [user1, user2]).save(failOnError: true)
+			place: place1,
+			title: "Fiesta1",
+			description: "...",
+			startDateTime: new Date(1429066800000L),
+			finsishDateTime: new Date(1429066900000L),
+			guests: [user1, user2]
+			).save(failOnError: true)
 
 		new Party(host: userm,
-		place: place1,
-		title: "Fiesta2",
-		description: "...",
-		startDateTime: new Date(1429066800000L),
-		finsishDateTime: new Date(1429066900000L),
-		guests: [user1, user2, userf]).save(failOnError: true)
+			place: place1,
+			title: "Fiesta2",
+			description: "...",
+			startDateTime: new Date(1429066800000L),
+			finsishDateTime: new Date(1429066900000L),
+			guests: [user1, user2, userf]
+			).save(failOnError: true)
 
 		new Party(host: userf,
-		place: place1,
-		title: "Fiesta3",
-		description: "...",
-		startDateTime: new Date(1429076800000L),
-		finsishDateTime: new Date(1429076900000L),
-		guests: [user1, user2]).save(failOnError: true)
+			place: place1,
+			title: "Fiesta3",
+			description: "...",
+			startDateTime: new Date(1429076800000L),
+			finsishDateTime: new Date(1429076900000L),
+			guests: [user1, user2]
+			).save(failOnError: true)
 		
 		new Party(host: userf,
-		place: place1,
-		title: "Fiesta4",
-		description: "...",
-		startDateTime: new Date(1429066800000L),
-		finsishDateTime: new Date(1429066900000L),
-		guests: [user1, user2, userm]).save(failOnError: true)
+			place: place1,
+			title: "Fiesta4",
+			description: "...",
+			startDateTime: new Date(1429066800000L),
+			finsishDateTime: new Date(1429066900000L),
+			guests: [user1, user2, userm]
+			).save(failOnError: true)
 	}
 
 	def destroy = {
