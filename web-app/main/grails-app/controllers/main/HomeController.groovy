4711b2bc3c4fadef02081e77b5ccd8361993384c
Fabricio PH <fabriph@gmail.com>
2015-01-31 15:00:04 -0300

    Creating new Users when needed.

--- web-app/main/grails-app/controllers/main/HomeController.groovy
+++ web-app/main/grails-app/controllers/main/HomeController.groovy
@@ -1,13 +1,26 @@
 package main
 
+import com.partytime.User
+import com.grailsrocks.authentication.AuthenticationUser
+
 class HomeController {
 
 	def authenticationService
 
 	def index() {
-//		if (!authenticationService.isLoggedIn(request)) {
-//			redirect(controller:"authentication", action:"index")
-//		}
+		if (!authenticationService.isLoggedIn(request)) {
+			redirect action:"login"
+			return
+		}
+		AuthenticationUser authUser = authenticationService.getUserPrincipal()
+		String login = authUser.getLogin()
+		if (!User.existsUser(login)) {
+			String email = authUser.getEmail()
+			User newUser = new User(name: login,
+			  bornDate: new Date(),
+			  mail: email).save(failOnError: true)
+		}
+		User.activeMyUser(login)
 		render view:"home.gsp"
 	}
 	
